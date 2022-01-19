03b79b834e17a6b854edc577748aeaf57d604550
Mohammad Jaha <miro.tune@gmail.com>
2022-01-19 10:32:59 +0000

    Finish Part 2

--- MusicApplication/Controlers/Other/WelcomeViewController.swift
+++ MusicApplication/Controlers/Other/WelcomeViewController.swift
@@ -8,11 +8,41 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    
+    private let signInButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Sign In With Spotify", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Spotify"
         view.backgroundColor = .green
+        
+        view.addSubview(signInButton)
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        signInButton.frame = CGRect(x: 20, y: view.height - 50 - view.safeAreaInsets.bottom, width: view.width - 40, height: 50)
+    }
+    
+    @objc func didTapSignIn() {
+        let vc = AuthViewController()
+        vc.completionHandler = { [weak self] success in
+            self?.handelSignIn(success: success)
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func handelSignIn(success: Bool) {
+        // Log User In
     }
 }
--- MusicApplication/Managers/AuthManager.swift
+++ MusicApplication/Managers/AuthManager.swift
@@ -10,7 +10,17 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "94c8741eddbc458c8b34f9a502f99909"
+        static let clientSecret = "417db148af22446a97fdc84046c979ac"
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let string = "https://accounts.spotify.com/authorize?response_type=code&client_id=\(Constants.clientID)&scope=user-read-private&redirect_uri=https://www.google.com.sa&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -32,5 +42,16 @@ final class AuthManager {
         return false
     }
     
+    public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)) {
+        //get Token
+        
+    }
     
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
+    }
 }
